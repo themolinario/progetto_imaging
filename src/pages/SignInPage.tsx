ab94dcdf8289b202fa6d86adfd46564f856c04d6
MarcoMolinari <themolinario@gmail.com>
2024-01-20 12:14:24 +0100

    Feature: validazione login

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -2,9 +2,13 @@ import {useEffect, useState} from "react";
 import {gapi} from "gapi-script";
 import {LoginButton} from "../components/LoginButton.tsx";
 import {AppBar, Box, Button, Container, TextField, Toolbar, Typography} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
 export function SignInPage () {
     const [medico, setMedico] = useState(false);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
     const clientId = "673042186693-2m5j2elqlhnukhflpjf4871goq384cb9.apps.googleusercontent.com";
 
     useEffect(() => {
@@ -17,6 +21,8 @@ export function SignInPage () {
 
         gapi.load('client:auth2', start);
     }, []);
+
+    const error = username !== "marco" || password !== "123456";
 
     return (
         <div>
@@ -38,10 +44,14 @@ export function SignInPage () {
                         <LoginButton />
                     </div>
                     <p>Sei un medico?</p>
-                    <Button onClick={() => setMedico(true)}>Effettua l'accesso come medico</Button>
-                    {medico && <TextField label="username"/>}
-                    {medico && <TextField label="password" type="password"/>}
-                    {medico && <Button>Login</Button>}
+                    <Button variant="contained" onClick={() => setMedico(true)}>Effettua l'accesso come medico</Button>
+                    {medico && <div style={{backgroundColor: "#ecebeb", borderRadius: 8, padding: 32, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+                        { <TextField label="username" value={username} onChange={(e) => setUsername(e.target.value)}/>}
+                        { <TextField label="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>}
+                        { <Button onClick={() => navigate("/home")} disabled={error}>Login</Button>}
+                        { error && <p style={{color: "red"}}>Credenziali errate!</p>}
+                    </div>}
+
                 </div>
 
             </Container>
